5d72073e14a6722ea3519654b1b9034b08ccee0b
ChrisJeffreys <ChrisBJeffreys@gmail.com>
2022-01-05 15:59:25 -0600

    add grid space click functionality

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GridSquare from './GridSquare'
 import styles from './Grid.module.css'
 
 export default function Grid() {
-  const grids = [...Array(9)].map((_, i) => <GridSquare key={i}/>)
+  const [player, setPlayer] = useState("Player 1")
+
+  const handleClick = () => {
+    setPlayer(player === "Player 1" ? "Player 2" : "Player 1")
+    console.log("Change Player")
+  }
+
+  const grids = [...Array(9)].map((_, i) => <GridSquare player={player} handleClick={handleClick} key={i}/>)
 
 
   return (
--- src/components/GridSquare.jsx
+++ src/components/GridSquare.jsx
@@ -1,9 +1,18 @@
 import React, { useState } from 'react'
 import styles from './GridSquare.module.css'
 
-export default function GridSquare() {
+export default function GridSquare(props) {
+  const [marker, setMarker] = useState("")
+
+  const handleClick = () => {
+    console.log("Clicked square")
+    setMarker(props.player === "Player 1" ? "X" : "O")
+    props.handleClick()
+  }
+
   return (
-    <div className={styles.square}>
+    <div className={styles.square} onClick={handleClick}>
+      {marker}
     </div>
   )
 }
